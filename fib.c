21ee86647c7ebb2d261825ab02cca095166901d2
Carson Christensen <carsonchristensen@Carsons-MacBook-Pro-3.local>
2024-09-26 20:40:23 -0700

    Finished

--- fib.c
+++ fib.c
@@ -1,52 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Recursive Fibonacci function
-int fib_recursive(int n) {
-   if(n == 0)
-      return 0
-   else if(n == 1)
-      return 1;
-   else
-      return (fib_recursive(n-1) + fib_recursive(n-2));
+// Original Recursive Fibonacci function (no changes)
+int fib_wrapper_recursive(int n) {
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
+    return fib_wrapper_recursive(n - 1) + fib_wrapper_recursive(n - 2);
 }
-//Ex: 8th Fibonacci: 8 == 0 NO, 8 == 1 NO, therefore ((8-1) + (8 -2)) = 13
 
-// Iterative Fibonacci function
-int fib_iterative(int n) {
-   int i, term_1 = 0, term_2 = 1, nextTerm;
-   for (i = 1; i <= n; ++i) {
-      printf("%d, ", term_1);
-      nextTerm = term_1 + term_2;
-      term_1 = term_2;
-      term_2 = nextTerm
-      return nextTerm
-   }
+// Original Iterative Fibonacci function (no changes)
+int fib_wrapper_iterative(int n) {
+    int term_1 = 0, term_2 = 1, nextTerm;
+    if (n == 0) return term_1;
+    if (n == 1) return term_2;
+
+    for (int i = 1; i < n; ++i) {
+        nextTerm = term_1 + term_2;
+        term_1 = term_2;
+        term_2 = nextTerm;
+    }
+    return term_2;
 }
-//Begins at 1 and will add the next term until it is equal to n terms.
-//Ex: 8th Fibonacci: term_1 + term_2 = 1, i < n, therefore loops again.
-//Will repeat loop 8 times until the correct number of sequences occurs. 
+
 int main(int argc, char *argv[]) {
-    int arg_value = atoi(argv[1]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <integer> <method> <filename>\n", argv[0]);
+        return 1;
+    }
 
-    FILE *file = fopen(argv[3], "r");
-    int file_value;
-    fscanf(file, "%d", &file_value);
+    int num_from_cli = atoi(argv[1]);
+    char method = argv[2][0];
+    char *filename = argv[3];
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", filename);
+        return 1;
+    }
+
+    int num_from_file;
+    fscanf(file, "%d", &num_from_file);
     fclose(file);
 
-    int N = arg_value + file_value - 1;
-   //Takes the first argument value and adds to the file value.
-   //Ex: Input 5 = arg_value + file_value = 3. 5 + 3 = 8-1 = 7.
-   //Therefore n = 7. 7th Fibonacci Number = 13.
+    int N = num_from_cli + num_from_file;
     int result;
-    if (argv[2][0] == 'r') {
-        result = fib_recursive(N);
+
+    if (method == 'r') {
+        result = fib_wrapper_recursive(N);
+    } else if (method == 'i') {
+        result = fib_wrapper_iterative(N);
     } else {
-        result = fib_iterative(N);
+        fprintf(stderr, "Error: Method must be 'r' (recursive) or 'i' (iterative)\n");
+        return 1;
     }
-   //Checks 2nd Command argument and will use either recursive or iterative to find solution
-    // Output the Nth fibonacci sequence
-    printf("%d\n", result);
+
+    printf("Fibonacci number at position %d is %d\n", N, result);
 
     return 0;
 }
